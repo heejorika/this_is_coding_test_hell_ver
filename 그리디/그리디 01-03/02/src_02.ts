d399bca9b8ff2f6b27139004f69b0e9c5a2f1cc7
song-wooseok <sws981202@gmail.com>
2023-01-15 07:08:53 +0900

    feat: 주어진 문자열에 숫자를 제외한 다른 문자가 있는지 검증하는 기능 구현

--- 그리디/그리디 01-03/02/src_02.ts
+++ 그리디/그리디 01-03/02/src_02.ts
@@ -2,6 +2,8 @@ const MAX_INPUT_LENGTH = 20;
 const MIN_INPUT_LENGTH = 1;
 
 class MaximumAccumulator{
+    private numbers:number[] = [];
+
     constructor(input:string){
         this.validateInput(input);
     }
@@ -14,5 +16,16 @@ class MaximumAccumulator{
         if(!((input.length >= MIN_INPUT_LENGTH) && (input.length <= MAX_INPUT_LENGTH))){
             throw new Error("옳바른 길이를 입력하세요."+MIN_INPUT_LENGTH +"이상, "+MAX_INPUT_LENGTH+"미만");
         }
+    }
+
+    private validateCorrectNumberInput(input:string){
+        [...input].forEach((value)=>{
+            let integer = parseInt(value);
+
+            if(isNaN(integer)){
+                throw new Error("숫자를 입력하세요! " + value + "는 숫자가 아닙니다.");
+            }
+            
+        })
     }
 }
