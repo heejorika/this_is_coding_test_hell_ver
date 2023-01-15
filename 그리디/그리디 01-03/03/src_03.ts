672983183fcb7ed40b18be192aa4d9653cac94af
song-wooseok <sws981202@gmail.com>
2023-01-15 19:21:14 +0900

    feat: 특정 문자가 몇번 연속되어 등장하는지 계산하는 기능 구현

--- 그리디/그리디 01-03/03/src_03.ts
+++ 그리디/그리디 01-03/03/src_03.ts
@@ -4,13 +4,22 @@ const MIN_CARD_LENGTH:number = 0;
 
 class CardFlipGame{
     private cardShapes:string[] = [];
+    private continuousCardCount:Map<string, number> = new Map();
 
     constructor(cards:string){
         this.validateCardInput(cards);
+        this.initCardCount();
+    }
+
+    private initCardCount():void{
+        CARD_SHAPE.forEach((cardShape)=>{
+            this.continuousCardCount.set(cardShape, 0);
+        })
     }
 
     private validateCardInput(cards:string): void{
         this.validateCardLength(cards);
+        this.validateCardShape(cards);
     }
 
     private validateCardLength(cards:string):void{
@@ -19,12 +28,45 @@ class CardFlipGame{
         }
     }
 
-    private validateCardShape(cards:string){
-        [...cards].forEach((cardShape)=>{
+    private validateCardShape(cards:string):void{
+        [...cards].forEach((cardShape:string)=>{
             if(!CARD_SHAPE.includes(cardShape)){
                 throw new Error("올바른 카드 모양을 입력해주세요");
             }
             this.cardShapes.push(cardShape);
         })
+    }
+
+    getMinumumFlipCount(){
+        this.countAllContinuousCardShape();
+    }
+
+    private countAllContinuousCardShape(){
+        let previousShape = this.cardShapes[0];
+
+        this.countContinuousCardShape(previousShape);
+
+        for(let index = 1; index<this.cardShapes.length; index++){
+            let currentShape = this.cardShapes[index];
+
+            if(currentShape != previousShape){
+                this.countContinuousCardShape(currentShape);
+            }
+            
+            previousShape = currentShape;
+        }
+    }
+
+    private countContinuousCardShape(cardShape:string):void{
+        this.continuousCardCount.set(cardShape, this.getContinuosCount(cardShape)+1);
+    }
+
+    private getContinuosCount(cardShape:string):number{
+        let count:number | undefined = this.continuousCardCount.get(cardShape);
+
+        if(count == undefined){
+            return 0;
+        }
+        return count;
     }
 }
